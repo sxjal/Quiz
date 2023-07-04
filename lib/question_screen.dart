fb41f6752d8c6faf0bf0bb5016b1952d909a45bb
sajal <sajalsahu8@gmail.com>
2023-07-05 03:47:10 +0400

    added questions data models

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:quizapp/background.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -13,6 +12,32 @@ class QuestionScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionScreen> {
   @override
   Widget build(context) {
-    return const Background(Text('Hey'));
+    return Column(
+      children: [
+        SizedBox(
+          height: .2 * MediaQuery.of(context).size.height,
+        ),
+        const Text("sajal"),
+        SizedBox(
+          height: .17 * MediaQuery.of(context).size.height,
+        ),
+        Row(
+          children: [
+            //answer 1
+            ElevatedButton(onPressed: () {}, child: const Text("Answer 1")),
+            //answer 2
+            ElevatedButton(onPressed: () {}, child: const Text("Answer 1")),
+          ],
+        ),
+        Row(
+          children: [
+            //answer 3
+            ElevatedButton(onPressed: () {}, child: const Text("Answer 3")),
+            //answer 4
+            ElevatedButton(onPressed: () {}, child: const Text("Answer 4")),
+          ],
+        ),
+      ],
+    );
   }
 }
