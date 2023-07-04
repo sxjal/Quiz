e43375f2a3ff26c9e452a5e248693884cd52edf4
sajal <sajalsahu8@gmail.com>
2023-07-05 01:56:46 +0400

    added colour to the screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,21 @@ import 'package:quizapp/screen.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Screen(),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.center,
+              end: Alignment.bottomLeft,
+              colors: [
+                Color.fromARGB(255, 93, 30, 186),
+                Color.fromARGB(255, 134, 67, 16),
+              ],
+            ),
+          ),
+          child: const Screen(),
+        ),
       ),
     ),
   );
