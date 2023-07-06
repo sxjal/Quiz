f66278de08affe6ef92cd5a9bc2a750e5b02fa2d
sajal <sajalsahu8@gmail.com>
2023-07-06 05:48:18 +0400

    shuffled the answers

--- lib/model/quiz_question.dart
+++ lib/model/quiz_question.dart
@@ -3,4 +3,10 @@ class QuizQuestion {
 
   final String text;
   final List<String> answers;
+
+  List<String> shuffledlist() {
+    final anotherlist = List.of(answers);
+    anotherlist.shuffle();
+    return anotherlist; 
+  }
 }
--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -18,26 +18,32 @@ class _QuestionScreenState extends State<QuestionScreen> {
   Widget build(context) {
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        //padding : const EdgeInsets.all(20),
-        children: [
-          Text(
-            currentquestion.text,
-            style: const TextStyle(
-              fontSize: 16,
-              color: Color.fromARGB(255, 194, 188, 188),
-              fontWeight: FontWeight.bold,
+      child: Container(
+        margin: const EdgeInsets.all(25),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+
+          //padding : const EdgeInsets.all(20),
+          children: [
+            Text(
+              currentquestion.text,
+              style: const TextStyle(
+                fontSize: 14,
+                color: Color.fromARGB(255, 234, 226, 226),
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
             ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          //generating buttons dynamically using the list and spread operator.
-          ...currentquestion.answers.map((value) {
-            return AnswerButton(buttontext: value, onTap: () {});
-          }),
-        ],
+            const SizedBox(
+              height: 30,
+            ),
+            //generating buttons dynamically using the list and spread operator.
+            ...currentquestion.shuffledlist().map((value) {
+              return AnswerButton(buttontext: value, onTap: () {});
+            }),
+          ],
+        ),
       ),
     );
   }
