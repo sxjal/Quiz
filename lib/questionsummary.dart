8fa99e2e824fc5c26f5b66347b7baed596187f85
sajal <sajalsahu8@gmail.com>
2023-07-06 20:27:48 +0400

    added new file question summary

---
+++ lib/questionsummary.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class QuestionSummary extends StatelessWidget {
+  const QuestionSummary({super.key, required this.data});
+  final List<Map<String, Object>> data;
+
+  @override
+  Widget build(context) {
+    return const Text('sajal');
+  }
+}
